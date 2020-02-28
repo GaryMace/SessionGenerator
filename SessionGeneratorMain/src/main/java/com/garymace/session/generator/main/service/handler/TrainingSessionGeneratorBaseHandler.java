0fdd324af7d5651d2388926fb05d66cc21efefb8
Gary Mac Elhinney <garymac@live.ie>
2020-02-28 23:02:22 +0000

    remove bs classes (#9)

--- SessionGeneratorMain/src/main/java/com/garymace/session/generator/main/service/handler/TrainingSessionGeneratorBaseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.garymace.session.generator.main.service.handler;
-
-import com.garymace.session.generator.base.models.profile.Profile;
-
-import java.util.Set;
-
-public interface TrainingSessionGeneratorBaseHandler<T> {
-    Set<T> handle(Profile profile);
-}
