221c6c4b12da478fbf9fc70fbbde8f83c463f1f1
GaryMace <gary-mac-elhinney@ucdconnect.ie>
2020-02-28 23:03:06 +0000

    remove bs classes

--- SessionGeneratorMain/src/main/java/com/garymace/session/generator/main/service/TrainingSessionGenerator.java
+++ SessionGeneratorMain/src/main/java/com/garymace/session/generator/main/service/TrainingSessionGenerator.java
@@ -1,34 +1,97 @@
 package com.garymace.session.generator.main.service;
 
-import com.garymace.session.generator.base.models.AthleticLevel;
 import com.garymace.session.generator.base.models.profile.Profile;
-import com.garymace.session.generator.base.models.session.brief.TrainingSessionBase;
-import com.garymace.session.generator.main.service.handler.TrainingSessionGeneratorBaseHandler;
+import com.garymace.session.generator.base.models.session.SessionSet;
+import com.garymace.session.generator.base.models.session.SessionStageDetails;
+import com.garymace.session.generator.base.models.session.SessionStageType;
+import com.garymace.session.generator.base.models.session.SetItem;
+import com.garymace.session.generator.base.models.session.brief.sport.SwimTrainingSession;
+import com.garymace.session.generator.base.models.session.builder.SessionBuilderAction;
+import com.garymace.session.generator.base.models.session.builder.SessionBuilderParams;
+import com.garymace.session.generator.base.models.session.rules.SessionRules;
+import com.garymace.session.generator.main.service.generator.rules.SessionRulesService;
 import com.google.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.handler.TrainingSessionGeneratorHandlerUtils;
 
-import java.util.Arrays;
-import java.util.Map;
+import utils.RandomUtils;
+import utils.session.SessionBuilderUtils;
+
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TrainingSessionGenerator {
     private static final Logger LOG = LoggerFactory.getLogger(TrainingSessionGenerator.class);
 
-    private final Map<AthleticLevel, TrainingSessionGeneratorBaseHandler> athleticLevelToSessionBriefHandlers;
+    private final SessionRulesService sessionRulesService;
 
     @Inject
-    public TrainingSessionGenerator(TrainingSessionGeneratorHandlerUtils trainingSessionGeneratorHandlerUtils) {
-        this.athleticLevelToSessionBriefHandlers = trainingSessionGeneratorHandlerUtils.getSessionBriefGeneratorHandlers();
+    public TrainingSessionGenerator(SessionRulesService sessionRulesService) {
+        this.sessionRulesService = sessionRulesService;
     }
 
-    public <T extends TrainingSessionBase> Set<T> generateFrom(Profile profile) {
-        if (!athleticLevelToSessionBriefHandlers.containsKey(profile.getAthleticLevel())) {
-            throw new RuntimeException(String.format("Unhandlable athletic type found; expected one of: %s, but got %s",
-                    Arrays.toString(AthleticLevel.values()), profile.getAthleticLevel()));
+    public Set<SwimTrainingSession> generateFrom(Profile profile) {
+        return generateTrainingSessions(profile);
+    }
+
+    private Set<SwimTrainingSession> generateTrainingSessions(Profile profile) {
+        return Stream.generate(() ->
+                SwimTrainingSession.builder()
+                        .setWarmupSessionStageDetails(generateSession(profile, SessionStageType.WARMUP))
+                        .setMainsetSessionStageDetails(generateSession(profile, SessionStageType.MAINSET))
+                        .setCooldownSessionStageDetails(generateSession(profile, SessionStageType.COOLDOWN))
+                        .build()
+        ).limit(profile.getWeeklySessionPreference())
+                .collect(Collectors.toSet());
+    }
+
+    private SessionStageDetails generateSession(Profile profile, SessionStageType sessionStageType) {
+        SessionRules sessionRules = sessionRulesService.getRules(profile, sessionStageType);
+        int sessionDistance = RandomUtils.getInRange(sessionRules.getMinDistance(), sessionRules.getMaxDistance());
+        int currentDistance = 0;
+        int currentSets = 0;
+
+        Set<SessionSet> sessionSets = new HashSet<>();
+        while (currentDistance < sessionDistance) {
+            LOG.info("Set's are currently: {}", sessionSets);
+            SessionBuilderAction sessionBuilderAction = SessionBuilderUtils.decide(SessionBuilderParams.builder()
+                    .setCurrentDistance(currentDistance)
+                    .setCurrentReps(currentSets)
+                    .setMaxDistance(sessionRules.getMaxDistance())
+                    .setMaxReps(sessionRules.getMaxReps())
+                    .build());
+
+            switch (sessionBuilderAction) {
+                case ADD_NEW_SESSION_SET:
+                    SessionSet sessionSet = SessionBuilderUtils.generateSessionSet(sessionRules);
+                    Optional<Integer> maybeDistance = sessionSet.getSetItems().stream()
+                            .map(SetItem::getDistance)
+                            .reduce(Integer::sum);
+                    if (!maybeDistance.isPresent()) {
+                        LOG.error("Unexpected error; couldn't calculate distance for SessionSet");
+                        continue;
+                    }
+                    currentDistance += (maybeDistance.get() * sessionSet.getSetReps());
+                    sessionSets.add(sessionSet);
+                    break;
+                case INCREASE_REPS:
+                    currentSets++;
+                    currentDistance *= 2;
+                    break;
+                case STOP:
+                    return SessionStageDetails.builder()
+                            .setSetCount(currentSets)
+                            .setSessionSets(sessionSets)
+                            .build();
+            }
         }
-        return athleticLevelToSessionBriefHandlers.get(profile.getAthleticLevel())
-                .handle(profile);
+        return SessionStageDetails.builder()
+                .setSetCount(currentSets)
+                .setSessionSets(sessionSets)
+                .build();
     }
 }
--- SessionGeneratorMain/src/main/java/com/garymace/session/generator/main/service/handler/SessionStageBaseHandler.java
+++
@@ -1,7 +0,0 @@
-package com.garymace.session.generator.main.service.handler;
-
-import com.garymace.session.generator.base.models.profile.Profile;
-
-public interface SessionStageBaseHandler<T> {
-    T handle(Profile profile);
-}
